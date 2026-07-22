2f44165d35470849c025790b9aa6b0ffc2c96e76
Bruno Vidal <brunovidal27.19@gmail.com>
2026-07-21 22:40:27 -0300

    fix: adicionar WhiteNoise para servir arquivos estáticos em produção
    - Instalado whitenoise>=6.0 no requirements.txt e pyproject.toml
    - Adicionado WhiteNoiseMiddleware após SecurityMiddleware
    - Corrigido STATIC_URL de 'static/' para '/static/'
    - Templates migrados de paths hardcoded para {% static %}

--- config/settings/base.py
+++ config/settings/base.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "apps.core.middleware.RequestIdMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -95,7 +96,7 @@ TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 
